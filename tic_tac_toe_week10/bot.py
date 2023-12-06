0459c6b6fcdf5815813dd5de2eed28723e7fae61
Jie(Jackie) Ji <35789241+jijie21@users.noreply.github.com>
2023-12-05 22:26:58 -0800

    Delete tic_tac_toe_week10 directory

--- tic_tac_toe_week10/bot.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# Implement a Bot class to handle the bot logic for the single-player mode
-class Bot:
-    def make_move(self, board):
-        available_moves = [(i, j) for i in range(3) for j in range(3) if board[i][j] is None]
-        row, col = random.choice(available_moves)
-        return f"{row},{col}"  # Return move as "row,col"
